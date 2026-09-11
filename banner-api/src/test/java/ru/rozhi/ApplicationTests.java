5bf4b460a7be2cb86980f8b74d8bd7f43640c606
nastyarozhina <nastarozina221@gmail.com>
2026-09-11 19:56:15 +0300

    Initial commit

---
+++ banner-api/src/test/java/ru/rozhi/ApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.rozhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
